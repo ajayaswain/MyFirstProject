51e79a9dfd11fb86160089c3953411362b335b05
ajaya <m1026124@A8MD21901.mindtree.com>
2014-11-21 18:45:43 +0530

    2 lines deleted

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -3,8 +3,7 @@ public static void main(String args[]){
 System.out.println("draft version");
 System.out.println("added in master branch");
 System.out.println("added in user1 branch");
-System.out.println("adding a line to check git config");
-System.out.println("added a new line to user1 branch------");
+
 
 
 }
