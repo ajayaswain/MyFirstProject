a72462368efe03f6f911e2288db8cc6bd93ee772
ajaya <m1026124@A8MD21901.mindtree.com>
2014-11-21 18:26:26 +0530

    new line --added

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -4,6 +4,7 @@ System.out.println("draft version");
 System.out.println("added in master branch");
 System.out.println("added in user1 branch");
 System.out.println("adding a line to check git config");
+System.out.println("added a new line to user1 branch------");
 
 
 }
