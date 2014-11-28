9b19511aa0a79b8099154153f7d4689e51fbe0dd
ajayaswain <ajaya.swain@gmail.com>
2014-11-28 11:26:36 +0530

    Remote_modification

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -3,6 +3,7 @@ public static void main(String args[]){
 System.out.println("draft version");
 System.out.println("added in master branch");
 System.out.println("added in user1 branch");
+// Added this line in remote user1 branch by using git hub page
 
 
 
