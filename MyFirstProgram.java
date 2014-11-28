652ff17520c8b3e953bd5adef7332fde683ff4f4
ajaya <m1026124@A8MD21901.mindtree.com>
2014-11-28 12:31:16 +0530

    resolved the conflicts

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -3,7 +3,11 @@ public static void main(String args[]){
 System.out.println("draft version");
 System.out.println("added in master branch");
 System.out.println("added in user1 branch");
+
 // Added this line in remote user1 branch by using git hub page
+
+System.out.println("adding a line to check git config");
+
 
 
 
